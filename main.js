c42635d4725a7395c4eb643652b683b6fb14be59
clarisse guiadem <clarisseguiadem071@gmail.com>
2023-04-22 12:47:30 +0200

    display category

--- main.js
+++ main.js
@@ -1,15 +1,43 @@
 const fetchUrl = "https://api.escuelajs.co/api/v1";
 
+let allCategory = [];
 
-    let allCategory = [];
 
 async function getCategory() {
     const response = await fetch(`${fetchUrl}/categories`)
     return await response.json();
 }
 
-async function displayAllCategory(){
-    allCategory = getCategory();
+async function displayAllCategory() {
+    allCategory = await getCategory();
 
     console.log(allCategory);
+
+    createCategoryCard();
+
+}
+
+displayAllCategory();
+
+const createCategoryCard = () => {
+
+    let categoryCard = document.querySelector(".cardDisplay");
+
+    allCategory.forEach(item => {
+const card = document.createElement("div");
+card.classList.add("card");
+
+const img = document.createElement("img");
+img.setAttribute("src", item.image);
+//img.setAttribute("alt", item.name);
+
+const h3 = document.createElement("h3");
+h3.textContent = item.name;
+
+card.appendChild(img);
+card.appendChild(h3);
+
+categoryCard.appendChild(card);
+
+    })
 }
